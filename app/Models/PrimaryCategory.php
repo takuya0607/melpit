87e1ad64a2792526f0b0d125cff9a2f2a315521e
takuya0607 <60646178+takuya0607@users.noreply.github.com>
2021-01-16 22:28:12 +0900

    商品出品機能の実装

--- app/Models/PrimaryCategory.php
+++ app/Models/PrimaryCategory.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class PrimaryCategory extends Model
 {
     //
+      public function secondaryCategories()
+    {
+      // 大カテゴリ(1)対小カテゴリ(多)の関係性
+      // 大カテゴリは1つしかないが、小カテゴリは分類化されて多数あるため
+      // これにより$category->secondaryCategoriesで大カテゴリに紐づく小カテゴリの一覧が取得できる
+      return $this->hasMany(SecondaryCategory::class);
+    }
 }
--- resources/views/sell.blade.php
+++ resources/views/sell.blade.php
@@ -64,7 +64,16 @@
                     <div class="form-group mt-3">
                         <label for="category">カテゴリ</label>
                         <select name="category" class="custom-select form-control @error('category') is-invalid @enderror">
-                            {{-- 次のパートで実装します --}}
+                          @foreach ($categories as $category)
+                            <optgroup label="{{$category->name}}">
+                              <!-- 大カテゴリに紐づく小カテゴリの一覧を取得 -->
+                              @foreach($category->secondaryCategories as $secondary)
+                                <option value="{{$secondary->id}}" {{old('category') == $secondary->id ? 'selected' : ''}}>
+                                  {{$secondary->name}}
+                                </option>
+                              @endforeach
+                            </optgroup>
+                          @endforeach
                         </select>
                         @error('category')
                         <span class="invalid-feedback" role="alert">
@@ -78,6 +87,12 @@
                         <label for="condition">商品の状態</label>
                         <select name="condition" class="custom-select form-control @error('condition') is-invalid @enderror">
                             {{-- 次のパートで実装します --}}
+                            @foreach ($conditions as $condition)
+                            <!-- old('condition') == $condition->id 前回選んだ商品状態を表示させる -->
+                            <option value="{{$condition->id}}" {{old('condition') == $condition->id ? 'selected' : ''}}>
+                              {{$condition->name}}
+                            </option>
+                          @endforeach
                         </select>
                         @error('condition')
                         <span class="invalid-feedback" role="alert">
